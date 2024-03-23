f7a618ed9d663579616b3d6e8322e8608c4509fc
Laura <chirchir0814@gmail.com>
2024-03-23 19:21:10 +0300

    update

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+const divide = function() {
+    return 2000 / 100;
+};
+
+const square = (num) => {
+    return num * num;
+};
+const add = (a, b) => {
+    return a + b;
+};
+console.log(divide()); 
+console.log( square(5)); 
+console.log( add(3, 7)); 
